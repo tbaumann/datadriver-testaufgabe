19b6d40fb757b3a7662d23fec9dcabdc6d86adaf
Tilman Baumann <tilman@baumann.name>
2024-12-03 19:20:36 +0100

    nix fmt

--- flake.nix
+++ flake.nix
@@ -3,19 +3,24 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem
-    (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
         pkgs = import nixpkgs {
           system = system;
-          config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
-            "terraform"
-          ];
+          config.allowUnfreePredicate = pkg:
+            builtins.elem (pkgs.lib.getName pkg) [
+              "terraform"
+            ];
         };
       in {
-        devShells.default = import ./shell.nix { inherit pkgs; };
+        devShells.default = import ./shell.nix {inherit pkgs;};
+        formatter = nixpkgs.legacyPackages.x86_64-linux.alejandra;
       }
     );
-    
 }
--- shell.nix
+++ shell.nix
@@ -1,16 +1,16 @@
-{ pkgs ? import <nixpkgs> { } }:
+{pkgs ? import <nixpkgs> {}}:
 with pkgs;
-mkShell {
-  buildInputs = [
-    awscli2
-    earthly
-    envsubst
-    k9s
-    kubectl
-    openssl
-    terraform
-  ];
+  mkShell {
+    buildInputs = [
+      awscli2
+      earthly
+      envsubst
+      k9s
+      kubectl
+      openssl
+      terraform
+    ];
 
-  shellHook = ''
-  '';
-}
+    shellHook = ''
+    '';
+  }
