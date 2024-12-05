20618d91add04d81e0593078280509d2576951fa
Tilman Baumann <tilman@baumann.name>
2024-12-05 14:17:59 +0100

    I confused refistry name with image name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,7 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+/*
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane"
   value       = module.eks.cluster_endpoint
@@ -20,3 +21,4 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = module.eks.cluster_name
 }
+*/
