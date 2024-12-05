fa50e4065dfef1b6ce354ff8e11d98e5e2f8c28b
Tilman Baumann <tilman@baumann.name>
2024-12-05 16:41:42 +0100

    Set up kube config

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,7 +52,7 @@ module "eks" {
   source = "terraform-aws-modules/eks/aws"
   #version = "20.8.5"
 
-  cluster_name    = local.cluster_name
+  cluster_name    = "datadrivers-demo"
   cluster_version = "1.31"
 
   cluster_endpoint_public_access           = true
