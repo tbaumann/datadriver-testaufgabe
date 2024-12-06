51140c1275650e08c01a0432338947488dfbed4c
Tilman Baumann <tilman@baumann.name>
2024-12-06 17:34:56 +0100

    Make App configurable at runtime and improve readyness probe

--- app/app.js
+++ app/app.js
@@ -1,8 +1,42 @@
 var express = require('express');
 
-const port = 8000;
-var app = express();app.get('/', function (req, res) {
-  res.send('Hello World!');
-});app.listen(port, function () {
+
+
+const Config = require('k8s-config');
+
+const config = new Config({addToEnv: true, optional: false});
+
+config.watch();
+
+
+const port = config.get('port', 8000);
+var ready = false;
+var app = express();
+app.get('/', function (req, res) {
+  var name = config.get('name', 'world');
+  res.send(`Hello ${name}!\n`);
+});
+
+// Simulate slow startup
+app.use('/ready', (request, response) => {
+  if (ready){
+    return response.sendStatus(200);
+  }else{
+    return response.sendStatus(500);
+  }
+});
+
+// Faaaake
+app.use('/live', (request, response) => {
+  return response.sendStatus(200);
+});
+
+
+app.listen(port, function () {
   console.log(`Serving on port ${port}`);
+  setTimeout(function() {
+      console.log("Setting ready flag")
+      ready = true;
+    }, 5000);
+
 });
