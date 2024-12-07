3e115cd4e24924bb1562f56be3ba91b0fecb3796
Tilman Baumann <tilman@baumann.name>
2024-12-07 01:05:12 +0100

    Cleanup comments

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -3,27 +3,18 @@
 
 terraform {
 
-  # cloud {
-  #   workspaces {
-  #     name = "learn-terraform-eks"
-  #   }
-  # }
-
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      #version = "~> 5.47.0"
     }
 
 
     tls = {
       source = "hashicorp/tls"
-      #      version = "~> 4.0.5"
     }
 
     cloudinit = {
       source = "hashicorp/cloudinit"
-      #version = "~> 2.3.4"
     }
   }
 
